fff34165c8c3baa2661e7ced902cc46247043447
leonplacko <58510062+leonplacko@users.noreply.github.com>
2023-05-18 00:21:33 +0200

    First commit, win forms and partial ui

---
+++ ScheduleGenWinForms/ScheduleGenWinForms/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ScheduleGenWinForms
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            InitWebview();
+        }
+
+        async void InitWebview()
+        {
+            await webView.EnsureCoreWebView2Async();
+            webView.CoreWebView2.Navigate(Path.Combine("file:", Application.StartupPath, "webapi", "index.html"));
+        }
+
+    }
+}
